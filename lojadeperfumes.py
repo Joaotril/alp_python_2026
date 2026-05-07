fd3fb6dc3c32710bcd4c1f8f63215991df590112
Joaotril <Joaopedroalveslima58@gmail.com>
2026-05-07 09:19:53 -0300

    Fix payment option comparison and formatting

--- lojadeperfumes.py
+++ lojadeperfumes.py
@@ -3,11 +3,11 @@ print('''1.À vista
 2.Cartão de débito
 3.Cartão de crédito''')
 pg = int(input('Forma de pagamento'))
-if pg == '1':
+if pg == 1:
     print(f'O valor total da compra é {valor-(valor*15/100)}')
-elif pg == '2':
+elif pg == 2:
     print(f'O valor total da compra é {valor-(valor*10/100)}')
-elif pg=='3':
+elif pg==3:
         print(f'O valor total da compra é {valor-(valor*5/100)}')
 else:
     print('Forma de pagamento inválida')
