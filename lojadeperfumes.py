969b05275337fdb67ee19bc6bfc2a9b4866ac769
Joaotril <Joaopedroalveslima58@gmail.com>
2026-05-07 09:20:53 -0300

    Fix input prompt for payment method

--- lojadeperfumes.py
+++ lojadeperfumes.py
@@ -2,7 +2,7 @@ valor = float(input('Digite o valor da compra'))
 print('''1.À vista
 2.Cartão de débito
 3.Cartão de crédito''')
-pg = int(input('Forma de pagamento'))
+pg = int(input('Forma de pagamento:'))
 if pg == 1:
     print(f'O valor total da compra é {valor-(valor*15/100)}')
 elif pg == 2:
