0a4b753faf9ce069622b8b469f3c6162dd2f9861
Joaotril <Joaopedroalveslima58@gmail.com>
2026-05-22 07:38:48 -0300

    calc

--- calculadorasimples.py
+++ calculadorasimples.py
@@ -1,26 +1,29 @@
-print('''Calculadora simples
-1.Soma
-2.Subtração
-3.Multiplicação
-4.Divisão
-0.Sair''')
-opcao = float(input('Selecione a operação: '))
-while opcao <0 or opcao > 4:
-    print('Opção inválida')
+import os
+while True:
+    print('''Calculadora simples
+    1.Soma
+    2.Subtração
+    3.Multiplicação
+    4.Divisão
+    0.Sair''')
     opcao = float(input('Selecione a operação: '))
-if opcao == 0:
-    print('Saindo...')
-    exit()
-n1 = float(input('Primeiro número: '))
-n2 = float(input('Segundo número: '))
-if opcao == 1:
-    print(f'O resultado da soma é {n1+n2}')
-elif opcao == 2:
-    print(f'O resultado da subtração é {n1-n2}')
-elif opcao == 3:
-    print(f'O resultado da multiplicação é {n1*n2}')
-elif opcao == 4:
-    if n2 == 0:
-        print('Essa divisão é impossivel')
-    else:
-        print(f'O resultado da divisão é {n1/n2}')
+    while opcao <0 or opcao > 4:
+        print('Opção inválida')
+        opcao = float(input('Selecione a operação: '))
+    if opcao == 0:
+        print('Saindo...')
+        break
+    n1 = float(input('Primeiro número: '))
+    n2 = float(input('Segundo número: '))
+    os.system('clear')
+    if opcao == 1:
+        print(f'O resultado da soma é {n1+n2}')
+    elif opcao == 2:
+        print(f'O resultado da subtração é {n1-n2}')
+    elif opcao == 3:
+        print(f'O resultado da multiplicação é {n1*n2}')
+    elif opcao == 4:
+        if n2 == 0:
+            print('Essa divisão é impossivel')
+        else:
+            print(f'O resultado da divisão é {n1/n2}')
